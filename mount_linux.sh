f7a9df5fff8e566199b6075210ca86fc7ac876ca
Daniel Warloch <35729024+DanielWarloch@users.noreply.github.com>
2020-03-29 00:14:23 +0100

    Update mount_linux.sh

--- mount_linux.sh
+++ mount_linux.sh
@@ -3,8 +3,7 @@
 #Path to mounting folder
 	#Example:
 	#	DIR="$HOME/SFTP"
-	
-DIR="$HOME/SFTP"
+	DIR="$HOME/SFTP"
 
 for disk in $(cat "$HOME/.config/rclone/rclone.conf" | grep -E '\[.*\]' | sed 's/\[//;s/\]//;s/\r$//' | tr -d '\r');
 do
